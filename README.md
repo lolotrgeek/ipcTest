6aa1813e8758a8fe384e84e0a63355e11fb91bbd
lolotrgeek <jon@simplyenable.com>
2022-03-12 09:54:50 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# IPCTest
+Tests IPC piping between node and python.
+
+[Reference.](https://levelup.gitconnected.com/inter-process-communication-between-node-js-and-python-2e9c4fda928d)
+
+### TODO
+- try julia
+
---
+++ pipe.js
@@ -0,0 +1,46 @@
+
+const fs = require('fs')
+const { spawn, fork } = require('child_process')
+
+const path_a = 'pipe_a'
+const path_b = 'pipe_b'
+
+let waiting = setInterval(() => {
+    try {
+        fs.accessSync(path_a)
+        clearInterval(waiting)
+        Write()
+    } catch (error) {
+        console.log('Waiting for python...')
+    }
+
+}, 1000)
+
+function Write() {
+    let fifo_b = spawn('mkfifo', [path_b])  // Create Pipe B
+    fifo_b.on('exit', function (status) {
+        console.log('Created Pipe B')
+
+        const fd = fs.openSync(path_b, 'r+')
+        let fifoRs = fs.createReadStream(null, { fd })
+        let fifoWs = fs.createWriteStream(path_a)
+
+        console.log('Ready to write')
+
+        setInterval(() => {
+            console.log('-----   Send packet   -----')
+            fifoWs.write(`${new Date().toISOString()}`)
+        }, 1000)  // Write data at 1 second interval
+
+        fifoRs.on('data', data => {
+
+            now_time = new Date()
+            sent_time = new Date(data.toString())
+            latency = (now_time - sent_time)
+
+            console.log('----- Received packet -----')
+            console.log('    Date   : ' + data.toString())
+            console.log('    Latency: ' + latency.toString() + ' ms')
+        })
+    })
+}
